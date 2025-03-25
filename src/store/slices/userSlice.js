3bb9e81cf7139145e88b763e647fde4f5ead874f
YuCheng07 <aa139532058@gmail.com>
2025-03-25 08:03:03 +0800

    fix:login default (#10)
    * feat:favorite (#7)
    
    * Feature/favorite (#9)
    
    * feat:favorite
    
    * fix:login default

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    isUserLogin: true,
+    isUserLogin: false,
     isLineLogin: false,
   },
   reducers: {
